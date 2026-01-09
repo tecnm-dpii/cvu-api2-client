d3b04ec5d3f155aafa3bfe894d93585aa65342a8
Francerz <10836837+francerz@users.noreply.github.com>
2026-01-09 16:00:51 -0600

    ADD resource TematicaResource with getAreas and getDisciplinas

---
+++ src/Resources/Catalogos/TematicaResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace TecNM_DPII\CvuApi2Client\Resources\Catalogos;
+
+use Francerz\JsonTools\JsonEncoder;
+use TecNM_DPII\CvuApi2Client\AbstractResource;
+use TecNM_DPII\CvuApi2Core\Models\Catalogos\AreaTematica;
+use TecNM_DPII\CvuApi2Core\Models\Catalogos\DisciplinaTematica;
+
+class TematicaResource extends AbstractResource
+{
+    /**
+     * @return AreaTematica[]
+     */
+    public function getAreas()
+    {
+        $response = $this->protectedGet('/catalogos/tematica-areas');
+        return JsonEncoder::decode((string)$response->getBody(), AreaTematica::class);
+    }
+
+    /**
+     * @return DisciplinaTematica[]
+     */
+    public function getDisciplinas()
+    {
+        $response = $this->protectedGet('/catalogos/tematica-disciplina');
+        return JsonEncoder::decode((string)$response->getBody(), DisciplinaTematica::class);
+    }
+}
